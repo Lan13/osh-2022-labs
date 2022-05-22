dad01be6928f7b93bb69f752a93c25f4aa817d37
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-22 13:55:13 +0800

    support for newline to send message

--- lab3/1.c
+++ lab3/1.c
@@ -13,13 +13,42 @@ struct Pipe {
 
 void *handle_chat(void *data) {
     struct Pipe *pipe = (struct Pipe *)data;
-    char buffer[1024] = "Message:";
+    char buffer[1024] = "";
+    
     ssize_t len;
     // recv 函数用来复制数据，将 fd_send 的内容复制到 buffer 当中
     // len 返回实际复制数据的字节数
-    while ((len = recv(pipe->fd_send, buffer + 8, 1000, 0)) > 0) { 
-        // send 函数用来发送数据，将 buffer 数据发送到 fd_recv
-        send(pipe->fd_recv, buffer, len + 8, 0);
+    while ((len = recv(pipe->fd_send, buffer, 1000, 0)) > 0) { 
+
+        // 将 buffer 得到的消息用 \n 进行分割
+        char **buffer_split = (char **)malloc(sizeof(char*) * 1024);
+        if (!buffer_split) {
+            perror("buffer_split malloc");
+        }
+
+        int message_cnt = 0;    // 记录有多少行消息
+        // 对每行消息分别进行分割复制
+        buffer_split[message_cnt] = strtok(buffer, "\n");
+        while (buffer_split[message_cnt]) {
+            message_cnt++;
+            buffer_split[message_cnt] = strtok(NULL, "\n");
+        }
+
+        // 将 prefix 和 suffix 加到每行消息并且发送
+        for (int i = 0; i < message_cnt; i++) {
+            char* send_message = (char*)malloc(sizeof(char) * (strlen(buffer_split[i]) + 36));
+            if (!send_message) {
+                perror("send_message malloc");
+            }
+            strcpy(send_message, "Message:");   // 添加 prefix
+            strcat(send_message, buffer_split[i]);  // 添加发送的消息内容
+            strcat(send_message, "\n");         // 添加 suffix
+
+            // send 函数用来发送数据，将 send_message 数据发送到 fd_recv
+            send(pipe->fd_recv, send_message, strlen(send_message), 0);
+            free(send_message);
+        }
+        free(buffer_split);
     }
     return NULL;
 }
