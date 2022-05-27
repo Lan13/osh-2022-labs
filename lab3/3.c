1ec430dd598a3f91258f75e14a266b4bde91c59a
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-27 21:29:13 +0800

    remove and restruct

--- lab3/3.c
+++ lab3/3.c
@@ -8,10 +8,9 @@
 #include <sys/time.h>
 #include <netinet/in.h>
 
-struct Pipe {
-    int fd_send;
-    int fd_recv;
-};
+#define MAX_MESSAGE_LEN 1048576
+#define MAX_MESSAGE_BUFFER_LEN 4096
+
 
 int main(int argc, char **argv) {
     int port = atoi(argv[1]);
@@ -33,38 +32,6 @@ int main(int argc, char **argv) {
         perror("listen");
         return 1;
     }
-    int fd1 = accept(fd, NULL, NULL);
-    int fd2 = accept(fd, NULL, NULL);
-    if (fd1 == -1 || fd2 == -1) {
-        perror("accept");
-        return 1;
-    }
-    fd_set clients;
-    char buffer[1024] = "Message:";
-    fcntl(fd1, F_SETFL, fcntl(fd1, F_GETFL, 0) | O_NONBLOCK); // 将客户端的套接字设置成非阻塞
-    fcntl(fd2, F_SETFL, fcntl(fd2, F_GETFL, 0) | O_NONBLOCK);
-    while (1) {
-        FD_ZERO(&clients);
-        FD_SET(fd1, &clients);
-        FD_SET(fd2, &clients);
-        if (select(fd2 + 1, &clients, NULL, NULL, NULL) > 0) { // 找出可以读的套接字
-            if (FD_ISSET(fd1, &clients)) {
-                ssize_t len = recv(fd1, buffer + 8, 1000, 0);
-                if (len <= 0) {
-                    break;
-                }
-                send(fd2, buffer, len + 8, 0);
-            }
-            if (FD_ISSET(fd2, &clients)) {
-                ssize_t len = recv(fd2, buffer + 8, 1000, 0);
-                if (len <= 0) {
-                    break;
-                }
-                send(fd1, buffer, len + 8, 0);
-            }
-        } else {
-            break;
-        }
-    }
+
     return 0;
 }
