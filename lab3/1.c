6f8ef563f476027df879d35d3ef42b49b8c9b910
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-29 09:43:20 +0800

    restruct 1.c again

--- lab3/1.c
+++ lab3/1.c
@@ -6,8 +6,7 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
-#define MAX_MESSAGE_LEN 1048576
-#define MAX_MESSAGE_BUFFER_LEN 4096
+#define MAX_MESSAGE_BUFFER_LEN 2048
 
 struct Pipe {
     int fd_send;
@@ -16,106 +15,57 @@ struct Pipe {
 
 void *handle_chat(void *data) {
     struct Pipe *pipe = (struct Pipe *)data;
-    // buffer 用来接受所有消息，最大不超过 1 MiB
-    char *buffer = (char*)malloc(sizeof(char) * MAX_MESSAGE_LEN);
-
-    while (1) { 
-        ssize_t len;
-        
-        // recv 函数用来复制数据，将 fd_send 的内容复制到 buffer 当中
-        // len 返回实际数据的字节数
+    char buffer[MAX_MESSAGE_BUFFER_LEN];
+    ssize_t len;
+    while (1) {
+        // recv之前需要 memset buffer
         memset(buffer, 0, sizeof(char) * MAX_MESSAGE_BUFFER_LEN);
         len = recv(pipe->fd_send, buffer, MAX_MESSAGE_BUFFER_LEN, 0);
         if (len <= 0) {
             break;
         }
-        // printf("len: %ld\n", len);
 
         // 将 buffer 得到的消息用 \n 进行分割
-        char **buffer_split = (char **)malloc(sizeof(char*) * 1024);
+        char **buffer_split = (char **)malloc(sizeof(char*) * MAX_MESSAGE_BUFFER_LEN);
         if (!buffer_split) {
             perror("buffer_split malloc");
         }
-
         int message_cnt = 0;    // 记录有多少行消息
         // 对每行消息分别进行分割复制
         buffer_split[message_cnt] = strtok(buffer, "\n");
         while (buffer_split[message_cnt]) {
             message_cnt++;
             buffer_split[message_cnt] = strtok(NULL, "\n");
-            // printf("buffer_split[message_cnt]: %s\n", buffer_split[message_cnt]);
         }
-
-        // printf("message_cnt: %d\n", message_cnt);
+        // 直接对换行符进行处理，因为如果只有一个\n时，strtok只会返回一个null
         if (message_cnt == 0) {
             send(pipe->fd_recv, "Message:\n", 9, 0);
         }
+
         // 将 prefix 和 suffix 加到每行消息并且发送
+        char *send_message = (char*)malloc(sizeof(char) * (MAX_MESSAGE_BUFFER_LEN + 9));
         for (int i = 0; i < message_cnt; i++) {
-            char *send_message = (char*)malloc(sizeof(char) * (strlen(buffer_split[i]) + 36));
+            int base_offset = 8;
+
             if (!send_message) {
                 perror("send_message malloc");
             }
             strcpy(send_message, "Message:");   // 添加 prefix
             strcat(send_message, buffer_split[i]);  // 添加发送的消息内容
-
-            // debug
-            // printf("loop i:%d send a message: %s", i, send_message);
-            // printf("loop i:%d message_cnt: %d\n", i, message_cnt);
-            
+            // 把原有的"\n"加回去，这里特殊判断比较多的原因是因为strtok()分割的性质
+            if (strlen(buffer_split[i]) < MAX_MESSAGE_BUFFER_LEN && (len != MAX_MESSAGE_BUFFER_LEN || i != message_cnt - 1)) {
+                strcat(send_message, "\n");
+                base_offset = 9;
+            }
             ssize_t send_len;
-            printf("send_message: %s", send_message);
-            while ((send_len = send(pipe->fd_recv, send_message, strlen(buffer_split[i]) + 8, 0)) < strlen(buffer_split[i]) + 8) {
+            while ((send_len = send(pipe->fd_recv, send_message, strlen(buffer_split[i]) + base_offset, 0)) < strlen(buffer_split[i]) + base_offset) {
+                printf("send twice!\n");
                 send_message = send_message + send_len;
-                printf("len: %ld, strlen(send_message): %ld", len, strlen(send_message));
             }
-            if (len < MAX_MESSAGE_BUFFER_LEN) {
-                send(pipe->fd_recv, "\n", 1, 0);
-            }
-            free(send_message);
         }
+        free(send_message);
         free(buffer_split);
-        while (len >= MAX_MESSAGE_BUFFER_LEN) {
-            memset(buffer, 0, sizeof(char) * MAX_MESSAGE_BUFFER_LEN);
-            len = recv(pipe->fd_send, buffer, MAX_MESSAGE_BUFFER_LEN, 0);
-            printf("len: %ld, buffer: %s", len, buffer);
-            char **buffer_split = (char **)malloc(sizeof(char*) * 1024);
-            int message_cnt = 0;    // 记录有多少行消息
-
-            
-            buffer_split[message_cnt] = strtok(buffer, "\n");
-            printf("buffer_split[%d]: %s\n", message_cnt, buffer_split[message_cnt]);
-            while (buffer_split[message_cnt]) {
-                message_cnt++;
-                buffer_split[message_cnt] = strtok(NULL, "\n");
-                // printf("buffer_split[message_cnt]: %s\n", buffer_split[message_cnt]);
-            }
-            if (message_cnt == 0) {
-                send(pipe->fd_recv, "Message:\n", 9, 0);
-            }
-            printf("after split message_cnt: %d\n", message_cnt);
-            for (int i = 0; i < message_cnt; i++) {
-                char *send_message = (char*)malloc(sizeof(char) * strlen(buffer_split[i]));
-                strcat(send_message, buffer_split[i]);  // 添加发送的消息内容
-                
-
-                ssize_t send_len;
-                // printf("send_message: %s", send_message);
-                while ((send_len = send(pipe->fd_recv, send_message, strlen(send_message), 0)) < strlen(send_message)) {
-                    send_message = send_message + send_len;
-                    printf("len: %ld, strlen(send_message): %ld", len, strlen(send_message));
-                }
-                if (i != message_cnt - 1) {
-                    send(pipe->fd_recv, "\nMessage:", 9, 0);
-                }
-                else {
-                    send(pipe->fd_recv, "\n", 1, 0);
-                }
-            }
-        }
     }
-
-    free(buffer);
     return NULL;
 }
 
